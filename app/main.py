1321e8c627fcd3c6915b6b6405850ebf60fb4759
aartik17 <aartik.1704@gmail.com>
2025-05-14 00:46:49 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,3 +9,6 @@ app.register_blueprint(inventory_bp)
 @app.route('/')
 def home():
     return "Welcome to TechNova Inventory Management System!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
