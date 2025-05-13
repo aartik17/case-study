b3e73dc3b73dede495672148301eb0b998e3400d
aartik17 <aartik.1704@gmail.com>
2025-05-14 00:09:00 +0530

    updated according to app/login.py in  main.py .

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from flask import Flask
+from login import login_bp
+
 app = Flask(__name__)
+app.register_blueprint(login_bp)
 
 @app.route('/')
 def home():
     return "Welcome to TechNova Inventory Management System!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
